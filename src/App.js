12c8e1a68fe845dc6c4fe9935ec3a77cea982696
Andrew Davis <andavi@users.noreply.github.com>
2021-06-09 06:18:52 -0700

    trying to switch views from welcoem screen to login screen - not working but taking break

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
-function App() {
+function Welcome(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>I-Ching</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The Book of Changes
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Consult the Oracle
-        </a>
-      </header>
+    <div className="Welcome">
+      <img src={logo} className="Welcome-logo" alt="logo" />
+      {/* <button className="App-button" onClick={ props.handleClick }>Login</button> */}
+      <Button variant="outline-light" block onClick={props.handleClick}>Login</Button>
     </div>
-  );
+  )
+}
+
+function Authenticate(props) {
+  return (
+    <h1>Welcome</h1>
+  )
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {beenWelcomed: false, isAuthenticated: false};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      beenWelcomed: true
+    }))
+  }
+  
+  render() {
+    return (
+      <div className="App">
+          <header className="App-header">
+            <h1>I-Ching</h1>
+              <p>
+                The Book of Changes
+              </p>
+          </header>
+          {this.state.beenWelcomed ? <Authenticate /> : <Welcome handleClick={this.props.handleClick} /> }
+      </div>
+    );
+  }
 }
 
 export default App;
